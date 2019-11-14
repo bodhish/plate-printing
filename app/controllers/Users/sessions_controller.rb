f4e2f420ae1ef51d18c45916df718cd99b32d350
Abdul Jaleel <abdul.jaleel@zomato.com>
2019-11-14 22:49:36 +0530

    Allow users to sign-in via password

--- app/controllers/Users/sessions_controller.rb
+++ app/controllers/Users/sessions_controller.rb
@@ -4,11 +4,22 @@ module Users
     # GET /user/sign_in
     def new
       if current_user.present?
-        flash[:notice] = 'You are already signed in.'
+        flash.now[:notice] = 'You are already signed in.'
         redirect_to after_sign_in_path_for(current_user)
       end
 
       @hide_navbar = true
+    end
+
+    def create
+      user = User.find_by(email: params[:user][:email])
+      if user && user.valid_password?(params[:user][:password])
+        sign_in user
+        redirect_to after_sign_in_path_for(user)
+      else
+        flash.now[:error] = 'Invalid username/password!'
+        render :new
+      end
     end
   end
 end
