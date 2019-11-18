71f1db9f8df6ef3baa700b1ad79a46866673ab4b
Abdul Jaleel <abdul.jaleel@zomato.com>
2019-11-18 23:10:12 +0530

    Add a customer filter and refactor filter bar

--- app/presenters/home/dashboard_presenter.rb
+++ app/presenters/home/dashboard_presenter.rb
@@ -1,7 +1,7 @@
 module Home
   class DashboardPresenter < ::ApplicationPresenter
     def date
-      params[:date_filter] ? Time.zone.strptime(params[:date_filter][:for_date], '%d-%m-%Y'): Time.zone.now
+      @date ||= params[:filter].try(:[],:date) ? Time.zone.strptime(params[:filter][:date], '%d-%m-%Y'): Time.zone.now
     end
 
     def monthly_stats
@@ -21,29 +21,33 @@ module Home
     end
 
     def jobs_on_day
-      PrintJob.where(created_at: date_window).includes(:customer, plate_jobs: :plate_dimension).order(created_at: :desc)
+     @jobs_on_day ||= PrintJob.where(customer_id: filtered_customer_ids).where(created_at: date_window).includes(:customer, plate_jobs: :plate_dimension).order(created_at: :desc)
+    end
+
+    def customer_dropdown_options
+      Customer.all.order(:name).map { |c| [c.name, c.id] }
     end
 
     private
 
     def jobs_in_month
-      PrintJob.where(created_at: month_window)
+      PrintJob.where(customer_id: filtered_customer_ids).where(created_at: month_window)
     end
 
     def plates_in_month
-      PlateJob.where(print_job: jobs_in_month).map { |pj| pj.set * pj.color }.sum
+      PlateJob.joins(:print_job).where(print_jobs: { customer_id: filtered_customer_ids }).where(print_job: jobs_in_month).map { |pj| pj.set * pj.color }.sum
     end
 
     def wastage_in_month
-      PlateJob.where(print_job: jobs_in_month).sum(:wastage)
+      PlateJob.joins(:print_job).where(print_jobs: { customer_id: filtered_customer_ids }).where(print_job: jobs_in_month).sum(:wastage)
     end
 
     def plates_on_day
-      PlateJob.where(print_job: jobs_on_day).map { |pj| pj.set * pj.color }.sum
+      PlateJob.joins(:print_job).where(print_jobs: { customer_id: filtered_customer_ids }).where(print_job: jobs_on_day).map { |pj| pj.set * pj.color }.sum
     end
 
     def wastage_on_day
-      PlateJob.where(print_job: jobs_on_day).sum(:wastage)
+      PlateJob.joins(:print_job).where(print_jobs: { customer_id: filtered_customer_ids }).where(print_job: jobs_on_day).sum(:wastage)
     end
 
     def date_window
@@ -52,6 +56,10 @@ module Home
 
     def month_window
       date.beginning_of_month..date.end_of_month
+    end
+
+    def filtered_customer_ids
+      @filtered_customer_ids ||= params[:filter].try(:[],:customer_id).present? ? params[:filter][:customer_id] : Customer.all.pluck(:id)
     end
   end
 end
