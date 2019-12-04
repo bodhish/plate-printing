63d5d3e0be78a1700794aed8b0c4c602f725f6fc
Abdul Jaleel <abdul.jaleel@zomato.com>
2019-12-04 19:28:52 +0530

    Allow editing is_admin from ActiveAdmin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,11 +1,12 @@
 ActiveAdmin.register User do
-  permit_params :email, :name, :password, :password_confirmation
+  permit_params :email, :name, :password, :password_confirmation, :is_admin
 
   index do
     selectable_column
     id_column
     column :email
     column :name
+    column :is_admin
     column :created_at
     actions
   end
@@ -21,6 +22,7 @@ ActiveAdmin.register User do
       f.input :name
       f.input :password
       f.input :password_confirmation
+      f.input :is_admin
     end
     f.actions
   end
